faa800054cdf6d48b13f94b9a0f9c678f19b89b1
Vladimir Maslenkov <maslenkov.vladimir@gmail.com>
2023-05-24 23:45:47 +0600

    added sections: promo, benefits, consultation and carousel. carousel section with slick-slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,15 @@
-
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        // adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../images/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../images/right.svg"></button>',
+        responsive: [{
+            breakpoint: 992,
+            settings: {
+              dots: true,
+              arrows: false
+            }
+        }]
+});
+  });
